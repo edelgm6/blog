ef4a247eb6b0c359625ea14173a810dac3ae9887
Garrett <edelgm6@gmail.com>
2020-05-13 18:31:19 -0700

    Removed permalinks, updated Babiators description

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -47,6 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'alabaster'
 html_sidebars = {'**': []}
+html_add_permalinks = ''
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
