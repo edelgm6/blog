5f0789b7c6a9f0adb080ac5db527b2fae45265b0
Garrett <edelgm6@gmail.com>
2020-05-29 14:44:33 -0700

    Overhaul Sphinx setup

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'garrett'
+project = 'gepage'
 copyright = '2020, Garrett Edel'
 author = 'Garrett Edel'
 
@@ -27,6 +27,8 @@ author = 'Garrett Edel'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -34,7 +36,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
